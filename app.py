17cbb573077ca15c2d1711ae41ff76b2e451b07a
briannesafire <briannesafire@yahoo.com>
2026-07-07 22:16:16 -0700

    Change app.run_server to app.run

--- app.py
+++ app.py
@@ -161,6 +161,7 @@ app.layout = html.Div([
 # Running the app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
